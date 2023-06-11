6a1d8fb61b8988ad9d1f35c0f563454d1d3b91fc
pattinsonfuture <pattinsonfuture@gmail.com>
2023-06-12 00:39:04 +0800

    調整Program Main流程

---
+++ Groundhog/Program.cs
@@ -0,0 +1,65 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Groundhog.Services;
+
+namespace Groundhog
+{
+    public class Program
+    {
+        private readonly PluginService _pluginService;
+        private readonly DatabaseService _databaseService;
+        public static IConfiguration Configuration { get; private set; }
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+
+                try
+                {
+                    Configuration = services.GetRequiredService<IConfiguration>();
+                    var client = new DiscordSocketClient();
+
+                    client.Log += Log;
+
+                    var token = Configuration["Token"];
+                    await client.LoginAsync(TokenType.Bot, token);
+                    await client.StartAsync();
+
+                    // 注册插件
+                    var databaseService = new DatabaseService("your_connection_string", "your_database_name", "your_collection_name");
+                    var pluginService = new PluginService(services);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.SetBasePath(AppContext.BaseDirectory);
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                    config.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true);
+                    config.AddEnvironmentVariables();
+                });
+
+        private static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Groundhog/src/Interfaces/IPlugin.cs
@@ -0,0 +1,20 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groundhog.Interfaces
+{
+    public interface IPlugin
+    {
+        // 在接口中定义插件所需的方法和属性
+        string Name { get; }
+        // 此Plugin是否啟動，function固定回傳bool
+        string GetName();
+        bool IsEnabled(ulong channelId);
+        // 定義任務類型
+        Task ExecuteAsync(SocketCommandContext context);
+    }
+}
